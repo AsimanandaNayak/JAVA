3910378ca90ea167a9c3e9ab35efe90f71729a94
Asima Nanda Nayak <149704517+AsimanandaNayak@users.noreply.github.com>
2023-11-23 00:43:26 +0530

    Add files via upload

---
+++ static_method.java
@@ -0,0 +1,12 @@
+public class static_method {
+    public static void main(String args[])
+    {
+        text.display();
+    }
+}
+class text {
+
+    public static void display(){
+        System.out.println("Asimananda");
+    }
+}
