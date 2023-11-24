2432fc2f0036601bad371447df3db1eaeb87846c
Asima Nanda Nayak <149704517+AsimanandaNayak@users.noreply.github.com>
2023-11-24 19:21:33 +0530

    Add files via upload

---
+++ qustion1.java
@@ -0,0 +1,15 @@
+public class qustion1 {
+        static double powerint(int n1,int n2){
+    return Math.pow(n1,n2);
+    }
+    static double powerdouble(double x,int  y){
+        return Math.pow(x,y);
+    }
+    public static void main(String args[])
+    {
+        System.out.println(powerint(10,2));
+        System.out.println(powerdouble(10.2,2));
+    }
+}
+
+
